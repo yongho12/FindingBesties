bef3cf8c1a233dcb6ae5acac5df8ea77cad79981
Yongho Kim <winthisgame12@gmail.com>
2020-11-12 21:51:07 -0700

    send message to friends

--- client/src/components/HomePage/Friends.js
+++ client/src/components/HomePage/Friends.js
@@ -8,11 +8,11 @@ function Friends()
 {
     const user_id = useSelector(state => state.authReducer.id);
     const fetchWithCSRF = useSelector(state => state.authReducer.csrf);
-    const [friends, setFriends] = useState([]);
-    const [msgButton, setMsgButton] = useState(false);
-    const [to_user, setTo_user] = useState(0)
-   
-    
+    const [ friends, setFriends ] = useState([]);
+    const [ msgButton, setMsgButton ] = useState(false);
+    const [ to_user, setTo_user ] = useState(0)
+    const [ message, setMessage ] = useState('')
+
     useEffect(() => {
         async function friendslist() {
             const response = await fetch(`/api/home/friendslist/${user_id}`)
@@ -30,10 +30,17 @@ function Friends()
 
     }
 
+    
+    const messageChange = (e) => {
+        setMessage(e.target.value)
+        
+    }
+    
     const messageSendHandle = async (e) => {
         e.preventDefault();
         console.log("message send e target value::::", e.target.value)
-        const message = 'this is the message'
+        // const message = 'this is the message'
+        console.log("message", message)
         const response = await fetchWithCSRF(`/api/home/friendsmessage/${user_id}`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
@@ -42,44 +49,52 @@ function Friends()
                message
             }),
         })
+        if (response.ok) {
+            window.alert("Message has sent successfully.")
+            setMessage('')
+        }
 
     }
-
 
 
 
     return (
     <>
     <h1>My Besties List</h1>
-        <div>
-            {friends.map((f, index)=>(<>
+        <div className = 'friends__container'>
+            {friends.map((f, index)=>(<div>
              <div key={`${f.id}-${index}`} >{f.friend_name} </div>
-             {/* <img className = "aaaa" src={f.friend_avatar} alt="friend photo" />    */}
+             <img className = "friends__photo" src={f.friend_avatar} alt="friend photo" />   
              <div>{f.friend_email}</div>
              <div>{f.status}</div> 
             <div>
                 <button value={f.id} onClick={messageHandle}>Message</button>
             </div>
-               </> ))}
-            {msgButton && <>
-            <form onSubmit={messageSendHandle}>
-                <label>
-                    Message:
-                    {/* <textarea value={} onChange={}/> */}
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
+               </div> ))}
 
-      
-      
-            {/* <textarea>Leave a message here</textarea>
-            <button onClick={messageSendHandle}>send!</button>  */}      
-            </>}
   
-        </div>    
+        </div>
+        <div>    
+            {msgButton && <>
+                 <input value={message}  type="text" placeholder="leave a message.." onChange={messageChange}></input>
+                 <a className="message--sent" onClick={messageSendHandle}>send</a>
+            </>}
+        </div>
     </>
     )
 }
+{/* <form onSubmit={messageSendHandle}>
+    <label>
+        Message:
+        <textarea onChange={messageChange}/>
+    </label>
+    <button type="submit">send</button>
+</form> */}
+
+
+
+{/* <textarea>Leave a message here</textarea>
+<button onClick={messageSendHandle}>send!</button>  */}      
 
 
 export default Friends;
