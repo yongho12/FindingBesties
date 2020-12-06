c1e922fa1afaf4922cb16d47feb29d131816c3f5
Yongho Kim <winthisgame12@gmail.com>
2020-12-05 22:58:26 -0700

    adding github icon

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import {AiOutlineGithub} from 'react-icons/fa'
+import { Link, NavLink, Switch, Route, Redirect} from 'react-router-dom'; 
+// import Link from 'react-redux';
+import { GrLinkedin , GrGithub} from "react-icons/gr";
+import styled, { css, ThemeProvider } from "styled-components";
+
 
 const Footer = () => {
     return (
         <div>
            <section id="contact" class="section">
-        <h1 class="contact__title">Let's talk</h1>
         <h2 class="contact__email">winthisgame12@gmail.com</h2>
         <div class="contact__links">
             {/* <a href="https://github.com/yongho12" target="_blank">
                 <i class="fab fa-github"></i>
             </a> */}
-            <AiOutlineGithub />
-            <a href="https://www.linkedin.com/in/yongho-kim-024451199/" target="_blank">
+            <GrGithub />
+            {/* <a href="https://www.linkedin.com/in/yongho-kim-024451199/" target="_blank">
                 <i class="fa fa-linkedin-square"></i>
-            </a>
+            </a> */}
+            <Link to="https://www.linkedin.com/in/yongho-kim-024451199/">
+                <GrLinkedin />
+            </Link>
         </div>
         <p class="contact__rights">
             2020 yonghokim.info - All rights reserved
